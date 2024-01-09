7111edc1d037f4d20a4c7ef93430bf5b398c8bfb
mac <uicodee@gmail.com>
2024-01-09 22:04:52 +0500

    upd

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,7 +9,7 @@ interface ButtonProps {
 
 export const Button: FC<ButtonProps> = ({children, type}) => {
     return (
-        <button className={`hidden md:flex px-8 py-3 border-gray-400 border rounded-md text-sm ${type === "primary" ? "bg-[#2b7f75] text-white border-0" : ""}`}>
+        <button className={`hidden md:flex px-8 py-3 border-gray-400 border rounded-md text-sm ${type === "primary" ? "bg-[#2b7f75] text-white border-0" : "text-neutral-600"}`}>
             {children}
         </button>
     );
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,7 +7,7 @@ export const Hero: FC = () => {
             <div className="flex items-center px-24 py-12 gap-x-2">
                 <div className="flex flex-col gap-y-8 w-1/2">
                     <h1 className="text-4xl font-semibold">Individually Designed</h1>
-                    <h1 className="text-4xl font-bold">Coaching Exclusively</h1>
+                    <h1 className="text-4xl font-semibold">Coaching Exclusively</h1>
                     <h1 className="text-4xl font-semibold">For You.</h1>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ad dolorem eum facilis harum libero non numquam similique ullam ut.</p>
                     <div className="flex gap-4 mt-4">
@@ -16,6 +16,7 @@ export const Hero: FC = () => {
                     </div>
                 </div>
                 <div className="flex flex-col gap-y-8 w-1/2">
+                    <div className="w-full bg-amber-300 h-20"></div>
                 </div>
             </div>
         </section>
